ef51c70cbb500ee48c4c845775101cbd5c497f5c
Casey <caseyclinga@gmail.com>
2019-07-18 18:47:09 -0700

    Undid heroku test

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -5,7 +5,7 @@ function Jumbotron() {
     return (
         <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Testing</h1>
+    <h1 class="display-4">React Book Search</h1>
         {/* Set ternery operator to render different text if user is on saved or search page */}
     <p class="lead">Search for and save books of interest</p>
   </div>
